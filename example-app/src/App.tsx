23172667dc07b1a5126b87b97d39fa98a472b490
Lucas Choque <lucas@openix.com.ar>
2024-01-24 17:37:28 -0300

    changes

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [hostname, setHostname] = useState('');
+
+  useEffect(() => {
+    // Get and set the hostname when the component mounts
+    const currentHostname = window.location.hostname;
+    setHostname(currentHostname);
+  }, []);
 
   return (
     <>
@@ -27,6 +35,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p className="read-the-docs">
+        {hostname}
       </p>
     </>
   )
